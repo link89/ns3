36144db7955c13482db4695345e113bdb3b7ae6f
Henry Xu <xuweihong.cn@gmail.com>
2015-04-02 13:17:56 +0800

    init commit: based on ns-allinone-3.22

---
+++ netanim-3.105/common.h
@@ -0,0 +1,48 @@
+#ifndef COMMON_H
+#define COMMON_H
+#include <stdint.h>
+#include <math.h>
+
+#include <QWidget>
+#include <QtGui/QToolButton>
+#include <QtGui/QSpinBox>
+#include <QtGui/QSlider>
+#include <QtGui/QLCDNumber>
+#include <QVBoxLayout>
+#include <QSplitter>
+#include <QToolBar>
+#include <QStatusBar>
+#include <QTime>
+#include <QThread>
+#include <QLabel>
+#include <QComboBox>
+#include <QProgressBar>
+#include <QGraphicsPixmapItem>
+#include <QParallelAnimationGroup>
+#include <QPropertyAnimation>
+#include <QPushButton>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QtCore/QXmlStreamReader>
+#include <QFile>
+#include <QTimer>
+#include <QDialog>
+#include <QApplication>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QGraphicsSceneHoverEvent>
+#include <QGraphicsProxyWidget>
+#include <QRegExp>
+#include <QFontMetrics>
+#include <QDesktopWidget>
+#include <QLineEdit>
+#include <QScrollBar>
+#include <QTableWidget>
+#include <QCheckBox>
+#include <QRegExp>
+
+
+#include "log.h"
+#include "fatal-error.h"
+
+#endif // COMMON_H
